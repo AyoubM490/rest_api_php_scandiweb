94e1b1703fd3cf9aa6bca5f5a677cb846d4d78f5
AyoubMS <chahayb563@gmail.com>
2022-09-30 14:55:54 +0100

    DVD endpoints In this commit, I added DVD endpoints

---
+++ api/dvd/create.php
@@ -0,0 +1,31 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../models/dvd.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$dvd = new DVD($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$dvd->sku = $data->sku;
+$dvd->name = $data->name;
+$dvd->price = $data->price;
+$dvd->size = $data->size;
+
+if ($dvd->create()) {
+    echo json_encode(
+        array("message" => "DVD was created.")
+    );
+} else {
+    echo json_encode(
+        array("message" => "DVD was not created.")
+    );
+}
---
+++ api/dvd/update.php
@@ -0,0 +1,32 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../models/dvd.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$dvd = new DVD($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$dvd->id = $data->id;
+$dvd->sku = $data->sku;
+$dvd->name = $data->name;
+$dvd->price = $data->price;
+$dvd->size = $data->size;
+
+if ($dvd->update()) {
+    echo json_encode(
+        array("message" => "DVD was updated.")
+    );
+} else {
+    echo json_encode(
+        array("message" => "DVD was not updated.")
+    );
+}
