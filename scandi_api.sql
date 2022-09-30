1878b487cb01e702a0d4c60f7ef24ba82bebeaa1
AyoubMS <chahayb563@gmail.com>
2022-09-30 14:40:56 +0100

    add file for DataBase creation

---
+++ scandi_api.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS `scandi_api`;
+
+USE `scandi_api`;
+CREATE TABLE `books` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `sku` varchar(255) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `price` decimal(10,2) NOT NULL,
+    `weight` decimal(10,2) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `dvds` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `sku` varchar(255) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `price` decimal(10,2) NOT NULL,
+    `size` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `furnitures` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `sku` varchar(255) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `price` decimal(10,2) NOT NULL,
+    `height` decimal(10,2) NOT NULL,
+    `width` decimal(10,2) NOT NULL,
+    `length` decimal(10,2) NOT NULL,
+    PRIMARY KEY (`id`)
+);
