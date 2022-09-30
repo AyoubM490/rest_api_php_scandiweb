3d126f2b9664def8bf5259de78be3f34bb5b0db8
AyoubMS <chahayb563@gmail.com>
2022-09-30 20:26:04 +0100

    fix bug

--- api/book/read_single.php
+++ api/book/read_single.php
@@ -3,7 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once '../../config/Database.php';
+include_once '../../config/config/Database.php';
 include_once '../../models/Book.php';
 
 $database = new Database();
--- api/dvd/read_single.php
+++ api/dvd/read_single.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-include_once '../../Database.php';
+include_once '../../config/Database.php';
 include_once '../../models/Dvd.php';
 
 $database = new Database();
