bb04ed1481866b5a35a4f15cb25d89cd27e31637
AyoubMS <chahayb563@gmail.com>
2022-09-30 23:07:59 +0100

    add validations

--- scandi_api.sql
+++ scandi_api.sql
@@ -1,29 +1,29 @@
 USE `heroku_34fe46c9712aa54`;
 CREATE TABLE `books` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `sku` varchar(255) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `price` decimal(10,2) NOT NULL,
-    `weight` decimal(10,2) NOT NULL,
+    `sku` varchar(255) NOT NULL CHECK (sku <> ''),
+    `name` varchar(255) NOT NULL CHECK (name <> ''),
+    `price` decimal(10,2) NOT NULL CHECK (price > 0),
+    `weight` decimal(10,2) NOT NULL CHECK (weight > 0),
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `dvds` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `sku` varchar(255) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `price` decimal(10,2) NOT NULL,
-    `size` varchar(255) NOT NULL,
+    `sku` varchar(255) NOT NULL CHECK (sku <> ''),
+    `name` varchar(255) NOT NULL CHECK (name <> ''),
+    `price` decimal(10,2) NOT NULL CHECK (price > 0),
+    `size` varchar(255) NOT NULL CHECK (size <> ''),
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `furnitures` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `sku` varchar(255) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `price` decimal(10,2) NOT NULL,
-    `height` decimal(10,2) NOT NULL,
-    `width` decimal(10,2) NOT NULL,
-    `length` decimal(10,2) NOT NULL,
+    `sku` varchar(255) NOT NULL CHECK (sku <> ''),
+    `name` varchar(255) NOT NULL CHECK (name <> ''),
+    `price` decimal(10,2) NOT NULL CHECK (price > 0),
+    `height` decimal(10,2) NOT NULL CHECK (height > 0),
+    `width` decimal(10,2) NOT NULL CHECK (width > 0),
+    `length` decimal(10,2) NOT NULL CHECK (length > 0),
     PRIMARY KEY (`id`)
 );
