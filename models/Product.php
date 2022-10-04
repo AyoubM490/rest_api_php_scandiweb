a4b3fa2d7ab962cbaed1271f41405209bf42fab9
AyoubMS <chahayb563@gmail.com>
2022-10-04 22:53:56 +0100

    fix bug

--- models/Product.php
+++ models/Product.php
@@ -83,7 +83,7 @@ class Product extends Products
             $params_string .= "'{$sku}',";
         }
         $params_string = trim($params_string, ",");
-        $prod_ids = $this->conn->query("SELECT `id` FROM `products` WHERE `sku` LIMIT 1 IN ({$params_string})")->fetchAll(PDO::FETCH_ASSOC);
+        $prod_ids = $this->conn->query("SELECT `id` FROM `products` WHERE `sku` IN ({$params_string}) LIMIT 1")->fetchAll(PDO::FETCH_ASSOC);
         if (count($prod_ids) > 0) {
             $params_string = "";
             foreach ($prod_ids as $prod) {
